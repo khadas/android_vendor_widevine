2171e55d0b286d4c0747f2ffd0c779359ead3c1a
shipeng.sun <shipeng.sun@amlogic.com>
2021-03-05 17:13:52 +0800

    widevine: Relase wv16 prebuilt files for Android R [1/2]
    PD#SWPL-44087
    
    Problem:
    Relase wv16 prebuilt files for Android R
    
    Solution:
    Relase wv16 prebuilt files for Android R
    
    Verify:
    android rr
    
    buildinfo:
    commit 254c9dbfcbd5c0aefb3fc7bab98d601cb3863d13 (HEAD, ohd/r-tv-dev, m/r-tv-dev)
    Author: jingjiangli <jingjiangli@google.com>
    Date:   Sun Jan 10 13:14:55 2021 -0800
    Allow priv-app to invoke signRSA API.
    commit 75145bfa9c4343a07f621ec9f6199ddb28d863ba
    Author: Fred Gylys-Colwell <fredgc@google.com>
    Date:   Fri Sep 11 15:00:15 2020 -0700
    Log X-Google fields on failed HTTP requests.
    
    Change-Id: If8ecc8e30ac97c9b2603db57576c8b3220072b57
    Signed-off-by: shipeng.sun <shipeng.sun@amlogic.com>

--- libwvdrmengine/Android.mk
+++ libwvdrmengine/Android.mk
@@ -2,18 +2,29 @@ LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/oemcryptolevel.mk
 ifneq ($(filter arm arm64, $(TARGET_ARCH)),)
 
-#####################################################################
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := move_widevine_data.sh
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE := $(LOCAL_SRC_FILES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := widevine
+
+include $(BUILD_PREBUILT)
+
 # -----------------------------------------------------------------------------
-# Builds android.hardware.drm@1.2-service.widevine
+# Builds android.hardware.drm@1.3-service.widevine
 #
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := android.hardware.drm@1.2-service.widevine
-LOCAL_INIT_RC := android.hardware.drm@1.2-service.widevine.rc
+LOCAL_MODULE := android.hardware.drm@1.3-service.widevine
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_INIT_RC := android.hardware.drm@1.3-service.widevine.rc
+LOCAL_VINTF_FRAGMENTS := manifest_android.hardware.drm@1.3-service.widevine.xml
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/bin/hw
 LOCAL_PROPRIETARY_MODULE := true
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_CHECK_ELF_FILES := false
 include $(BUILD_PREBUILT)
 
 # libwvhidl.so
@@ -28,6 +39,7 @@ LOCAL_PROPRIETARY_MODULE := true
 LOCAL_STRIP_MODULE := false
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
+LOCAL_CHECK_ELF_FILES := false
 include $(BUILD_PREBUILT)
 
 endif # TARGET_ARCH == arm
