785cdb58f100144c365f80ad47995b1989557467
Bian Jin chen <kenjc.bian@rock-chips.com>
2021-07-22 01:49:37 +0000

    L3: build widevine with s-fs-release latest code.
    commit 7d91a78cb5ca1f72897850017c5949a7db39ab81 (HEAD, origin/s-fs-release)
    Merge: 8defa9de 18be0939
    Author: Android Build Coastguard Worker <android-build-coastguard-worker@google.com>
    Date:   Thu Jul 15 01:18:34 2021 +0000
    
        Snap for 7550575 from 18be093969225a464309f294eac68169dcb6dd29 to sc-release
    
        Change-Id: I84280c0aa0e853c49d9b61bab3aedd717f10b8a6
    
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I9665d6a1a025f201a4fefc7a026b9c372dcb9989

--- bin/move_widevine_data.sh
+++ bin/move_widevine_data.sh
@@ -9,8 +9,8 @@ if [ ! -f "$FILES_MOVED" ]; then
   for i in "$SRC_PATH/IDM"*; do
     dest_path=$DEST_PATH/"${i#$SRC_PATH/}"
     if [ -d "$i" ]; then
-      mkdir -p $dest_path -m 700
-      mv $i "$DEST_PATH"
+      mkdir -p "$dest_path" -m 700
+      mv "$i" "$DEST_PATH"
       for file in ${dest_path}/${L3_FILE_PATTERN}; do
         [ -e "${file}" ] || continue
         # We need to move any L3 files in the IDM folder to the L3 subfolder.
@@ -18,7 +18,7 @@ if [ ! -f "$FILES_MOVED" ]; then
         mkdir -p "${l3_dir}" -m 700
         mv "${file}" "${l3_dir}"
       done
-      find $dest_path -print0 | while IFS= read -r -d '' file
+      find "$dest_path" -print0 | while IFS= read -r -d '' file
         do
           chgrp media "$file"
         done
